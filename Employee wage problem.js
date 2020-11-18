1752ca30ac5e9d372b0fcf511a0e9c8e74c06187
tejaswishah1 <tejaswishah1998@gmail.com>
2020-11-18 10:44:12 +0530

    [Tejas] add Check attendance

--- Employee wage problem.js
+++ Employee wage problem.js
@@ -1,1 +1,12 @@
-console.log('Employee wage problem using JS');
+//UC-1-Check Employee Absent/Present Using Random
+
+const IS_ABSENT = 0;
+
+let empCheck = Math.floor(Math.random()*10)%2;
+if(empCheck == IS_ABSENT){
+    console.log("Employee is absent");
+    return;
+}
+else {
+    console.log("Employee is present");
+}
