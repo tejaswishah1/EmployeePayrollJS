a53501f40c011b5ec27f911770dedf8b76eea99b
tejaswishah1 <tejaswishah1998@gmail.com>
2020-11-18 10:48:32 +0530

    [Tejas] add Check monthly wage

--- Employee wage problem.js
+++ Employee wage problem.js
@@ -1,4 +1,4 @@
-//UC-3-Using Function to Check Emp Hours
+//UC4: Monthly wage
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -17,8 +17,15 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empCheck = Math.floor(Math.random()*10)%3;
-let empHrs = getWorkingHours(empCheck);
-let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Emp hours: "+empHrs);
-console.log("Emp wage: "+empWage);
+const NUM_OF_WORKING_DAYS = 20;
+
+let totalEmpHrs = 0;
+for(day=0;day<NUM_OF_WORKING_DAYS;day++){
+    let empCheck = Math.floor(Math.random()*10)%3;
+    totalEmpHrs = totalEmpHrs+getWorkingHours(empCheck);
+}
+
+let empWage = totalEmpHrs*WAGE_PER_HOUR;
+
+console.log("Emp hours: "+totalEmpHrs);
+console.log("Emp wage: "+empWage)
