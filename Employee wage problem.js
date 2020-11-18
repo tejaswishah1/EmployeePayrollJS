b95cfbca50335ac2bea298c7e8ef9cb91607c2d1
tejaswishah1 <tejaswishah1998@gmail.com>
2020-11-18 10:47:17 +0530

    [Tejas] add Check wage using function

--- Employee wage problem.js
+++ Employee wage problem.js
@@ -1,15 +1,24 @@
-//UC-2-Calculate Daily Wage of Employee
+//UC-3-Using Function to Check Emp Hours
 
-let empHrs = 0;
-switch(empCheck){
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+function getWorkingHours(empCheck) {
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
+
+let empCheck = Math.floor(Math.random()*10)%3;
+let empHrs = getWorkingHours(empCheck);
 let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Emp wage = "+empWage)
+console.log("Emp hours: "+empHrs);
+console.log("Emp wage: "+empWage);
